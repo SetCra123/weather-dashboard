b3a52b606b298bcb3b24fbad556f7e5a11ef3365
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-07-11 23:17:30 -0400

    I got the weather information to show up on the dashboard

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,4 @@
-const API_key = '6ec5094f4d65c50536ef3931117576bf';
+const API_KEY = '6ec5094f4d65c50536ef3931117576bf';
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const storedCities = JSON.parse(localStorage.getItem('cities')) || [];
@@ -8,91 +8,73 @@ const btnContainer = document.getElementById('buttons');
 console.log(container);
 
 function getData(city) {
+    if (!city) {
+        city = searchInput.value.trim();
+    }
 
-    if  (!city) {
-         city = searchInput.value.trim();
-    } 
-    const units = "imperial";
-    const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${API_key}`;
+    const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
     fetch(apiURL)
-    .then(response => response.json()) 
-    .then(data => {
-        renderCurrentWeather(city, data);
-        storeSearchHistory(city);
-    })
-    
-    
-        
-    
-    // .catch(error => {
-    //   console.error(error);
-    // });
-
+        .then(response => response.json())
+        .then(data => {
+            renderCurrentWeather(city, data);
+            storeSearchHistory(city);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
-
-function renderCurrentWeather(city, weather){
-    
+function renderCurrentWeather(city, weather) {
     container.innerHTML = '';
     const cityName = weather.city.name;
     const temp = weather.list[0].main.temp;
     const wind = weather.list[0].wind.speed;
     const humid = weather.list[0].main.humidity;
     const icon = weather.list[0].weather[0].icon;
-    const iconurl = `http://openweathermap.org/img/wn/${icon}.png`
+    const iconurl = `http://openweathermap.org/img/wn/${icon}.png`;
 
-    
     const tempH1 = document.createElement('h1');
     const windH1 = document.createElement('h1');
     const humidH1 = document.createElement('h1');
     const iconImg = document.createElement('img');
 
-tempH1.textContent = `Temperature: ${temp}°F`;
-windH1.textContent = `Wind: ${wind} m/s`;
-iconImg.setAttribute('src', iconurl);
-humidH1.textContent = `Humidity: ${humid}%`;
-container.append(iconImg, tempH1, windH1, humidH1);
+    tempH1.textContent = `Temperature: ${temp}°K`;
+    windH1.textContent = `Wind: ${wind} m/s`;
+    humidH1.textContent = `Humidity: ${humid}%`;
+    iconImg.setAttribute('src', iconurl);
 
-createHistoryButton();
+    container.append(iconImg, tempH1, windH1, humidH1);
 
+    createHistoryButton();
 }
+
 function storeSearchHistory(city) {
     if (!storedCities.includes(city)) {
         storedCities.push(city);
-        localStorage.setItems('cities', JSON.stringify(storedCities));
+        localStorage.setItem('cities', JSON.stringify(storedCities));
     }
-
 }
 
 function createHistoryButton() {
     btnContainer.innerHTML = '';
     const thisStoredCity = JSON.parse(localStorage.getItem('cities')) || [];
     thisStoredCity.forEach(city => {
-      
         const newButton = document.createElement('button');
-        newButton.classList.add('historybtn')
+        newButton.classList.add('historybtn');
         newButton.textContent = city;
         btnContainer.append(newButton);
     });
-    
 }
-    
 
 function handleSearchHistory(e) {
-        if(!e.target.matches('.historybtn')){
+    if (!e.target.matches('.historybtn')) {
         return;
     }
     const target = e.target;
     const cityName = target.textContent;
     getData(cityName);
-
 }
-
-
-
 
 createHistoryButton();
 btnContainer.addEventListener('click', handleSearchHistory);
-
-
 searchButton.addEventListener('click', () => getData());
